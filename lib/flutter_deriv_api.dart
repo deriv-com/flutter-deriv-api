d3dcdb553e8aef7fa5b7379f7232c36b4b0071aa
Morteza Tavanarad <morteza@binary.com>
2019-12-08 22:02:42 +0800

    Morteza/fix_build_error (#4)
    https://trello.com/c/NgcDFA0V/233-05-clean-up-the-flutter-deriv-api-code-05
    
    * Cleaned up login.dart
    
    * ignore ios/android from the repo
    
    * Ignore lib/generated and test from analysing check
    
    * Fixed linter issues
    
    * Added missed autogenerated data calsses
    
    * Ignore coverage directory
    
    * move login.dart to widgets directory
    
    * Remove python and svn configs from .gitignore

--- lib/flutter_deriv_api.dart
+++ lib/flutter_deriv_api.dart
@@ -1,11 +1,10 @@
-import 'dart:async';
-
 import 'package:flutter/services.dart';
-import 'package:flutter_deriv_api/api/api.dart';
 
+/// The main class of FlutterDerivApi.
 class FlutterDerivApi {
   static const MethodChannel _channel = MethodChannel('flutter_deriv_api');
 
+  /// The version of current platform
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
