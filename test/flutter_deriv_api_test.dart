a3be5cd0ef93f225f2328236dc1287f9fd72102c
Ramin <ramin.vakili@firstsource.io>
2020-03-19 09:48:17 +0330

    added another step to build process

--- test/flutter_deriv_api_test.dart
+++ test/flutter_deriv_api_test.dart
@@ -3,9 +3,13 @@ import 'package:flutter_deriv_api/api/authorize_receive.dart';
 import 'package:flutter_deriv_api/api/authorize_send.dart';
 import 'package:flutter_deriv_api/api/p2p_advert_list_receive.dart';
 import 'package:flutter_deriv_api/api/p2p_advert_list_send.dart';
+import 'package:flutter_deriv_api/api/p2p_order_info_receive.dart';
+import 'package:flutter_deriv_api/api/p2p_order_info_send.dart';
 import 'package:flutter_deriv_api/api/p2p_order_list_receive.dart';
 import 'package:flutter_deriv_api/api/p2p_order_list_send.dart';
 import 'package:flutter_deriv_api/api/response.dart';
+import 'package:flutter_deriv_api/api/website_status_receive.dart';
+import 'package:flutter_deriv_api/api/website_status_send.dart';
 import 'package:flutter_deriv_api/connection/connection_websocket.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_deriv_api/flutter_deriv_api.dart';
@@ -39,12 +43,14 @@ void main() {
     final P2pOrderListResponse orderListResponse = await api.call(
         'p2p_order_list', P2pOrderListRequest(offset: 0, limit: 10));
 
-    print('Test result: orderList length: ${orderListResponse.p2pOrderList.length}');
+    print(
+        'Test result: orderList length: ${orderListResponse.p2pOrderList.length}');
 
     final P2pAdvertListResponse advertListResponse =
         await api.call('p2p_advert_list', P2pAdvertListRequest());
 
-    print('Test result: advertList length: ${advertListResponse.p2pAdvertList.length}');
+    print(
+        'Test result: advertList length: ${advertListResponse.p2pAdvertList.length}');
 
     api
         .subscribe('p2p_order_list', P2pOrderListRequest())
@@ -52,7 +58,17 @@ void main() {
       print('');
     });
 
-    await Future<void>.delayed(const Duration(seconds: 2));
+    final P2pOrderInfoResponse orderInfoResponse = await api.call(
+      'p2p_order_info',
+      P2pOrderInfoRequest(id: orderListResponse.p2pOrderList['list'][0]['id']),
+    );
 
+    print('Test result: Order info: ${orderInfoResponse.p2pOrderInfo}');
+
+    final WebsiteStatusResponse websiteStatusResponse =
+        await api.call('website_status', WebsiteStatusRequest());
+    print('Test result: Website status: ${websiteStatusResponse.websiteStatus}');
+
+    await Future<void>.delayed(const Duration(seconds: 2));
   });
 }
