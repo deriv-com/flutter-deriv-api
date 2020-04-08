03477c2af405384a071dd913290ea8b2be2f27cf
hamed-fs <hamed@firstsource.io>
2020-04-08 11:45:27 +0430

    hamed/add_call_to_api_call_manager
    - add call to api call manager

--- lib/connection/api_call_manager.dart
+++ lib/connection/api_call_manager.dart
@@ -1,7 +1,12 @@
+import 'package:flutter_deriv_api/api/request.dart';
+
 /// Api call manager abstract class
-abstract class ApiCallManager {
+abstract class ApiCallManager<T> {
   /// Indicates that pending request queue contain a request with [requestId] or not
   bool contains(int requestId);
+
+  /// Calls the api method
+  T call(Request request);
 
   /// Handle call response
   void handleResponse({
