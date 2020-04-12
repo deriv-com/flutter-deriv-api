c6a6c091f3713637c0716698533fb75e7640f8cc
hamed-fs <hamed@firstsource.io>
2020-04-12 09:55:26 +0430

    hamed/refactor_api_builder
    - refactor api builder

--- lib/api/request.dart
+++ lib/api/request.dart
@@ -1,13 +1,15 @@
-import 'dart:convert';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'request.g.dart';
 
-/// Super-class of all requests
+/// Super class of all requests
 @JsonSerializable(nullable: false, fieldRename: FieldRename.snake)
 class Request {
   /// Initializes
-  Request({this.passthrough, this.reqId});
+  Request({
+    this.passthrough,
+    this.reqId,
+  });
 
   /// Creates instance from JSON
   factory Request.fromJson(Map<String, dynamic> json) =>
@@ -15,10 +17,10 @@ class Request {
 
   /// [Optional] Used to pass data through the websocket, which may be
   /// retrieved via the `echo_req` output field.
-  Map<String, dynamic> passthrough;
+  final Map<String, dynamic> passthrough;
 
   /// [Optional] Used to map request to response.
-  int reqId;
+  final int reqId;
 
   /// Converts to JSON
   Map<String, dynamic> toJson() => _$RequestToJson(this);
--- lib/api/response.dart
+++ lib/api/response.dart
@@ -1,29 +1,33 @@
-import 'dart:convert';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'response.g.dart';
 
-/// Super-class of all requests
+/// super class of all requests
 @JsonSerializable(nullable: false, fieldRename: FieldRename.snake)
 class Response {
   /// Initializes
-  Response({this.echoReq, this.error, this.msgType, this.reqId});
+  Response({
+    this.echoReq,
+    this.error,
+    this.msgType,
+    this.reqId,
+  });
 
   /// Creates instance from JSON
   factory Response.fromJson(Map<String, dynamic> json) =>
       _$ResponseFromJson(json);
 
   /// Echo of the request made.
-  Map<String, dynamic> echoReq;
+  final Map<String, dynamic> echoReq;
 
   /// Error
-  Map<String, dynamic> error;
+  final Map<String, dynamic> error;
 
   /// Action name of the request made.
-  String msgType;
+  final String msgType;
 
   /// [Optional] Used to map request to response.
-  int reqId;
+  final int reqId;
 
   /// Converts to JSON
   Map<String, dynamic> toJson() => _$ResponseToJson(this);
