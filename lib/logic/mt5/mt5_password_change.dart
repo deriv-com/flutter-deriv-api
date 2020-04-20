ef8b8a824681787d8cc1e1f8b8b8ec194529136d
hamed-fs <hamed@firstsource.io>
2020-04-20 12:00:21 +0430

    hamed/add_mt5_password_change_class
    - add mt5 password change class

---
+++ lib/logic/mt5/mt5_password_change.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_deriv_api/models/mt5/mt5_password_change.dart';
+
+/// MT5 password change class
+class MT5PasswordChange extends MT5PasswordChangeModel {
+  /// Class constructor
+  MT5PasswordChange({
+    int mt5PasswordChange,
+  }) : super(
+          mt5PasswordChange: mt5PasswordChange,
+        );
+
+  /// Creates instance from json
+  factory MT5PasswordChange.fromJson(Map<String, dynamic> json) =>
+      MT5PasswordChange(
+        mt5PasswordChange: json['mt5_password_change'],
+      );
+
+  /// Creates copy of instance with given parameters
+  MT5PasswordChange copyWith({
+    int mt5PasswordChange,
+  }) =>
+      MT5PasswordChange(
+        mt5PasswordChange: mt5PasswordChange ?? this.mt5PasswordChange,
+      );
+}
---
+++ lib/models/mt5/mt5_password_change.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_deriv_api/models/base_model.dart';
+
+/// MT5 password change model class
+abstract class MT5PasswordChangeModel extends BaseModel {
+  /// Class constructor
+  MT5PasswordChangeModel({
+    this.mt5PasswordChange,
+  });
+
+  /// `1` on success
+  final int mt5PasswordChange;
+}
