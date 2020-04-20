ddf8f38b662ad2f086e216a0d422bdaddae6aace
hamed-fs <hamed@firstsource.io>
2020-04-20 12:10:44 +0430

    hamed/add_mt5_withdrawal_class
    - add mt5 withdrawal class

---
+++ lib/logic/mt5/mt5_withdrawal.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_deriv_api/models/mt5/mt5_withdrawal.dart';
+
+/// MT5 withdrawal class
+class MT5Withdrawal extends MT5WithdrawalModel {
+  /// Class constructor
+  MT5Withdrawal({
+    int mt5Withdrawal,
+    int binaryTransactionId,
+  }) : super(
+          mt5Withdrawal: mt5Withdrawal,
+          binaryTransactionId: binaryTransactionId,
+        );
+
+  /// Creates instance from json
+  factory MT5Withdrawal.fromJson(Map<String, dynamic> json) => MT5Withdrawal(
+        mt5Withdrawal: json['mt5_withdrawal'],
+        binaryTransactionId: json['binary_transaction_id'],
+      );
+
+  /// Creates copy of instance with given parameters
+  MT5Withdrawal copyWith({
+    int mt5Withdrawal,
+    int binaryTransactionId,
+  }) =>
+      MT5Withdrawal(
+        mt5Withdrawal: mt5Withdrawal ?? this.mt5Withdrawal,
+        binaryTransactionId: binaryTransactionId ?? this.binaryTransactionId,
+      );
+}
---
+++ lib/models/mt5/mt5_withdrawal.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_deriv_api/models/base_model.dart';
+
+/// MT5 withdrawal model class
+abstract class MT5WithdrawalModel extends BaseModel {
+  /// Class constructor
+  MT5WithdrawalModel({
+    this.mt5Withdrawal,
+    this.binaryTransactionId,
+  });
+
+  /// `1` on success
+  final int mt5Withdrawal;
+
+  /// Deposit reference ID of Binary account.
+  final int binaryTransactionId;
+}
