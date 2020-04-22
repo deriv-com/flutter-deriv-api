981ae47604d324da965425bc41854999f7a49b99
hamed-fs <57184669+hamed-fs@users.noreply.github.com>
2020-04-22 14:20:55 +0430

    hamed/add_trading_classes (#39)
    * hamed/add_trading_durations_class
    
    - add trading durations class
    - add trading durations unit test
    
    * hamed/add_trading_times_classes
    
    - add trading times classes
    - add trading times unit test
    
    * hamed/minor_refactor
    
    - minor refactor

---
+++ lib/api/models/symbol_model.dart
@@ -0,0 +1,62 @@
+import 'package:flutter_deriv_api/api/models/base_model.dart';
+import 'package:flutter_deriv_api/api/models/trade_event_model.dart';
+import 'package:flutter_deriv_api/api/models/trade_times_model.dart';
+
+/// Symbol class
+class SymbolModel extends BaseModel {
+  /// Class constructor
+  SymbolModel({
+    this.displayName,
+    this.name,
+    this.events,
+    this.symbol,
+    this.times,
+  });
+
+  /// Creates instance from json
+  factory SymbolModel.fromJson(Map<String, dynamic> json) => SymbolModel(
+        displayName: json['display_name'],
+        name: json['name'],
+        events: json['events'] == null
+            ? null
+            : json['events']
+                .map<TradeEventModel>(
+                    (dynamic item) => TradeEventModel.fromJson(item))
+                .toList(),
+        symbol: json['symbol'],
+        times: json['times'] == null
+            ? null
+            : TradeTimesModel.fromJson(json['times']),
+      );
+
+  /// Translated symbol name.
+  final String displayName;
+
+  /// Symbol name.
+  final String name;
+
+  /// Events
+  final List<TradeEventModel> events;
+
+  /// Symbol
+  final String symbol;
+
+  /// Trade Times
+  final TradeTimesModel times;
+
+  /// Creates copy of instance with given parameters
+  SymbolModel copyWith({
+    String displayName,
+    String name,
+    List<TradeEventModel> events,
+    String symbol,
+    TradeTimesModel times,
+  }) =>
+      SymbolModel(
+        displayName: displayName ?? this.displayName,
+        name: name ?? this.name,
+        events: events ?? this.events,
+        symbol: symbol ?? this.symbol,
+        times: times ?? this.times,
+      );
+}
