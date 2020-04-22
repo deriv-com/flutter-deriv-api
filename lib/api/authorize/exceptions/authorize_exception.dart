577939a1987df6a56bd2a10793cfeac4d58f3b83
hamed-fs <hamed@firstsource.io>
2020-04-22 08:55:13 +0430

    hamed/minor_refactor
    - minor refactor

--- lib/api/authorize/exceptions/authorize_exception.dart
+++ lib/api/authorize/exceptions/authorize_exception.dart
@@ -1,8 +1,4 @@
 import 'package:flutter_deriv_api/api/exceptions/api_base_exception.dart';
 
 /// Authorize exception class
-class AuthorizeException implements APIBaseException {
-  @override
-  String get message =>
-      '${AuthorizeException().runtimeType} Exception: $message';
-}
+class AuthorizeException extends APIBaseException {}
