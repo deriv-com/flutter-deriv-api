982bae20fcb117e8bc87f3632eb02b78a38d0bfc
hamed-fs <hamed@firstsource.io>
2020-04-22 09:44:17 +0430

    hamed/minor_refactor
    - minor refactor

--- lib/api/forget/exceptions/forget_exception.dart
+++ lib/api/forget/exceptions/forget_exception.dart
@@ -1,4 +1,7 @@
 import 'package:flutter_deriv_api/api/exceptions/api_base_exception.dart';
 
 /// Forget exception class
-class ForgetException extends APIBaseException {}
+class ForgetException extends APIBaseException {
+  /// Class constructor
+  ForgetException({String message}) : super(message: message);
+}
