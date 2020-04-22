be91eddd46b5a432638145cf92ee4fcdea3d8616
hamed-fs <hamed@firstsource.io>
2020-04-22 09:43:27 +0430

    hamed/minor_refactor
    - minor refactor

--- lib/api/authorize/exceptions/authorize_exception.dart
+++ lib/api/authorize/exceptions/authorize_exception.dart
@@ -1,4 +1,7 @@
 import 'package:flutter_deriv_api/api/exceptions/api_base_exception.dart';
 
 /// Authorize exception class
-class AuthorizeException extends APIBaseException {}
+class AuthorizeException extends APIBaseException {
+  /// Class constructor
+  AuthorizeException({String message}) : super(message: message);
+}
