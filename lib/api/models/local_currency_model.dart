25672a08ad110c3082002db06cfd87870010b054
hamed-fs <57184669+hamed-fs@users.noreply.github.com>
2020-04-23 14:53:56 +0430

    hamed/add_authorize_class (#36)
    * hamed/add_authorize_class
    
    - add authorize class
    
    * hamed/add_base_model
    
    - add base model
    
    * hamed/minor_refactor
    
    - minor refactor
    
    * hamed/add_unit_test_to_authorize_class
    
    - add unit test to authorize class
    
    * hamed/add_date_time_helper
    
    - add date time helper
    
    * hamed/minor_refactor
    
    - minor refactor
    
    * hamed/minor_refactor
    
    - minor refactor
    
    * hamed/minor_refactor
    
    - minor refactor
    
    * hamed/minor_refactor
    
    - minor refactor
    
    * hamed/sort_imports
    
    - sort imports
    
    * hamed/fix_imports
    
    - fix imports
    
    * hamed/minor_refactor
    
    - minor refactor

---
+++ lib/api/models/local_currency_model.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_deriv_api/api/models/base_model.dart';
+
+/// Local currency model class
+class LocalCurrencyModel extends BaseModel {
+  /// Class constructor
+  LocalCurrencyModel({
+    this.currencyCode,
+    this.fractionalDigits,
+  });
+
+  /// Generate instance from json
+  factory LocalCurrencyModel.fromJson(Map<String, dynamic> json) =>
+      LocalCurrencyModel(
+        currencyCode: json['key'],
+        fractionalDigits: json['values']['fractional_digits'],
+      );
+
+  /// Currency code
+  final String currencyCode;
+
+  /// Number of fractional digits.
+  final int fractionalDigits;
+}
---
+++ test/api/models/authorize_model_test.dart
@@ -0,0 +1,56 @@
+import 'dart:convert';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_deriv_api/api/authorize/account.dart';
+import 'package:flutter_deriv_api/api/authorize/authorize.dart';
+import 'package:flutter_deriv_api/api/models/local_currency_model.dart';
+
+import 'authorize_model_mock_data.dart';
+
+void main() {
+  test('authorize json parsing', () {
+    final Map<String, dynamic> mapData = jsonDecode(authorizeModelMockData);
+    final Authorize authorize = Authorize.fromJson(mapData['authorize']);
+    final List<Account> accounts = authorize.accountList;
+    final List<LocalCurrencyModel> localCurrencies = authorize.localCurrencies;
+
+    expect(accounts.length, 2);
+
+    expect(accounts[0].currency, 'USD');
+    expect(accounts[1].excludedUntil,
+        DateTime.fromMillisecondsSinceEpoch(1587486726000));
+    expect(accounts[0].isDisabled, false);
+    expect(accounts[0].isVirtual, false);
+    expect(accounts[0].landingCompanyName, 'svg');
+    expect(accounts[0].loginid, 'CR90000028');
+
+    expect(accounts[1].currency, 'USD');
+    expect(accounts[1].excludedUntil,
+        DateTime.fromMillisecondsSinceEpoch(1587486726000));
+    expect(accounts[1].isDisabled, false);
+    expect(accounts[1].isVirtual, true);
+    expect(accounts[1].landingCompanyName, 'virtual');
+    expect(accounts[1].loginid, 'VRTC90000028');
+
+    expect(localCurrencies.length, 2);
+
+    expect(localCurrencies[0].currencyCode, 'ZAR');
+    expect(localCurrencies[0].fractionalDigits, 2);
+
+    expect(localCurrencies[1].currencyCode, 'USD');
+    expect(localCurrencies[1].fractionalDigits, 3);
+
+    expect(authorize.balance, 10000);
+    expect(authorize.country, 'za');
+    expect(authorize.currency, 'USD');
+    expect(authorize.email, 'hamed1@binary.com');
+    expect(authorize.fullname, 'Ms QA script hamedSTX');
+    expect(authorize.isVirtual, false);
+    expect(authorize.landingCompanyFullname, 'Binary (SVG) Ltd.');
+    expect(authorize.landingCompanyName, 'svg');
+    expect(authorize.loginid, 'CR90000028');
+    expect(authorize.scopes.length, 4);
+    expect(authorize.upgradeableLandingCompanies.length, 1);
+    expect(authorize.userId, 29);
+  });
+}
