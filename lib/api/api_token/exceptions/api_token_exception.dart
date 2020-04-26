6580221b97c2668b67d20b0fb972df1955c81aad
hamed-fs <hamed@firstsource.io>
2020-04-26 14:55:39 +0430

    hamed/minor_refactor
    - minor refactor

--- lib/api/api_token/exceptions/api_token_exception.dart
+++ lib/api/api_token/exceptions/api_token_exception.dart
@@ -1,8 +1,7 @@
 import 'package:flutter_deriv_api/api/exceptions/api_base_exception.dart';
 
 /// Api token exception class
-class APITokenException implements APIBaseException {
-  @override
-  String get message =>
-      '${APITokenException().runtimeType} Exception: $message';
+class APITokenException extends APIBaseException {
+  /// Class constructor
+  APITokenException({String message}) : super(message: message);
 }
