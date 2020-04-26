590dc84e0a5a805d452fe502b079bede5b3e4a7a
hamed-fs <hamed@firstsource.io>
2020-04-26 13:40:10 +0430

    hamed/add_update_contract_class
    - add update contract class
    - add update contract unit test

---
+++ lib/api/models/spot_price_model.dart
@@ -0,0 +1,47 @@
+import 'package:flutter_deriv_api/api/models/base_model.dart';
+import 'package:flutter_deriv_api/utils/helpers.dart';
+
+/// Stop loss model class
+class SpotPriceModel extends BaseModel {
+  /// Class constructor
+  SpotPriceModel({
+    this.displayName,
+    this.orderAmount,
+    this.orderDate,
+    this.value,
+  });
+
+  /// Generate instance from json
+  factory SpotPriceModel.fromJson(Map<String, dynamic> json) => SpotPriceModel(
+        displayName: json['display_name'],
+        orderAmount: json['order_amount'],
+        orderDate: getDateTime(json['order_date']),
+        value: json['value'],
+      );
+
+  /// Localized display name
+  final String displayName;
+
+  /// Stop loss amount
+  final double orderAmount;
+
+  /// Stop loss order epoch
+  final DateTime orderDate;
+
+  /// Stop loss pip-sized barrier value
+  final String value;
+
+  /// Generate copy of instance with given parameters
+  SpotPriceModel copyWith({
+    String displayName,
+    double orderAmount,
+    DateTime orderDate,
+    String value,
+  }) =>
+      SpotPriceModel(
+        displayName: displayName ?? this.displayName,
+        orderAmount: orderAmount ?? this.orderAmount,
+        orderDate: orderDate ?? this.orderDate,
+        value: value ?? this.value,
+      );
+}
