2c7767787ca7bf08fb3d5f38de0f7ea60a5d3c49
Ramin Vakili <ramin.vakili@firstsource.tech>
2020-04-26 12:49:38 +0430

    added get account settings classes

---
+++ test/api/account_settings/account_settings_mock_data.dart
@@ -0,0 +1,15 @@
+const String accountSettingsJSON = '''{
+  "echo_req": {
+    "get_settings": 1
+  },
+  "get_settings": {
+    "country": "Afghanistan",
+    "country_code": "af",
+    "email": "ramin.vakili@firstsource.io",
+    "email_consent": 1,
+    "has_secret_answer": 0,
+    "residence": "Afghanistan",
+    "user_hash": "b1258cd44e55d7d736cacc30c37ec2218defd17c44635e61a78ddac6ca07bd31"
+  },
+  "msg_type": "get_settings"
+}''';
---
+++ test/api/account_settings/account_settings_test.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_deriv_api/api/account_settings/account_settings.dart';
+import 'account_settings_mock_data.dart';
+
+void main() {
+  test('Account settings JSON parsing', () {
+    final Map<String, dynamic> accountSettingsMap =
+        jsonDecode(accountSettingsJSON);
+    final AccountSettings accountSettings =
+        AccountSettings.fromJson(accountSettingsMap['get_settings']);
+
+    expect(accountSettings.country, 'Afghanistan');
+    expect(accountSettings.countryCode, 'af');
+    expect(accountSettings.userHash,
+        'b1258cd44e55d7d736cacc30c37ec2218defd17c44635e61a78ddac6ca07bd31');
+  });
+}
