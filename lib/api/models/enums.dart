c66ebdf2814d0bdef8d991ae7109839a7b8405a8
hamed-fs <57184669+hamed-fs@users.noreply.github.com>
2020-04-27 12:07:39 +0430

    hamed/add_api_token_class (#32)
    * hamed/add_api_token_class
    
    - add api token class
    
    * hamed/add_base_model
    
    - add base model
    
    * hamed/minor_refactor
    
    - minor refactor
    
    * hamed/rename_model_files
    
    - rename model files
    
    * hamed/add_unit_test_to_api_token
    
    - add unit test to api token
    - add enum helper
    
    * hamed/resolve_build_issue
    
    - resolve build issue
    
    * hamed/minor_refactor
    
    - minor refactor
    
    * hamed/minor_refactor
    
    - minor refactor

--- lib/api/models/enums.dart
+++ lib/api/models/enums.dart
@@ -1,1 +1,18 @@
-// enums of model classes
+// Api token
+/// Api token scopes
+enum TokenScope {
+  /// Read Access
+  read,
+
+  /// Trade Access
+  trade,
+
+  /// Trading Information Access
+  tradingInformation,
+
+  /// Payments Access
+  payments,
+
+  /// Admin Access
+  admin,
+}
