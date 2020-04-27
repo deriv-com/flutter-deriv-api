0066bd290aa3dcf7f5e26d54f62767029fe20213
hamed-fs <57184669+hamed-fs@users.noreply.github.com>
2020-04-27 12:07:39 +0430

    hamed/add_api_token_class (#32)
    * hamed/add_api_token_class
    
    - add api token class
    
    * hamed/add_base_model
    
    - add base model
    
    * hamed/minor_refactor
    
    - minor refactor
    
    * hamed/rename_model_files
    
    - rename model files
    
    * hamed/add_unit_test_to_api_token
    
    - add unit test to api token
    - add enum helper
    
    * hamed/resolve_build_issue
    
    - resolve build issue
    
    * hamed/minor_refactor
    
    - minor refactor
    
    * hamed/minor_refactor
    
    - minor refactor

---
+++ test/api/models/api_token_model_mock_data.dart
@@ -0,0 +1,17 @@
+const String apiTokenModelMockData = '''
+{
+  "api_token": {
+    "new_token": 1,
+    "tokens": [
+      {
+        "display_name": "sample_token",
+        "last_used": "2020-01-11",
+        "scopes": ["read", "trading_information"],
+        "token": "thisIsASampleTOKEN123",
+        "valid_for_ip": "178.32.12.45"
+      }
+    ]
+  },
+  "msg_type": "api_token"
+}
+''';
