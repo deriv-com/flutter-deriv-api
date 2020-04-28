0ae1ddcd0b4879881803227a8fb27e336b4182db
Ramin Vakili <ramin.vakili@firstsource.tech>
2020-04-28 09:57:33 +0430

    added model classes for TnC, email verify, cashier url

---
+++ test/api/models/cashier_url_mock_data.dart
@@ -0,0 +1,6 @@
+const String cashierURLJSON = '''{
+  "echo_req": {},
+  "msg_type": "cashier",
+  "cashier": "http://example.url/sagsjgajshkjHSas",
+  "req_id": 681
+}''';
---
+++ test/api/models/tnc_approval_mock_data.dart
@@ -0,0 +1,6 @@
+const String tncApprovalJSON = '''{
+  "echo_req": {},
+  "msg_type": "tnc_approval",
+  "tnc_approval": 1,
+  "req_id": 681
+}''';
