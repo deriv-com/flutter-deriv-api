c4bbee6ffad047bdedc681ee67ef77f7d66ee0b8
Ramin Vakili <ramin.vakili@firstsource.tech>
2020-04-29 16:22:05 +0430

    fix exception class

--- lib/api/copy_trading/exceptions/copy_trading_exception.dart
+++ lib/api/copy_trading/exceptions/copy_trading_exception.dart
@@ -2,7 +2,6 @@ import '../../exceptions/api_base_exception.dart';
 
 /// Exception for CopyTrading
 class CopyTradingException extends APIBaseException {
-  @override
-  String get message =>
-      '${CopyTradingException().runtimeType} Exception: $message';
+  /// Initializes
+  CopyTradingException({String message}) : super(message: message);
 }
