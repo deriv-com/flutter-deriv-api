d2fb62881b3bafeb4f6a2e07dc605f3cd4be66fb
hamed-fs <hamed@firstsource.io>
2020-04-29 09:15:49 +0430

    hamed/minor_refactor
    - minor refactor

--- lib/api/models/copy_trading_start_model.dart
+++ lib/api/models/copy_trading_start_model.dart
@@ -5,23 +5,23 @@ import 'package:flutter_deriv_api/utils/helpers.dart';
 class CopyTradingStartModel extends BaseModel {
   /// Initializes
   CopyTradingStartModel({
-    this.copyStart,
+    this.succeeded,
   });
 
   /// Generate an instance from json
   factory CopyTradingStartModel.fromJson(Map<String, dynamic> json) =>
       CopyTradingStartModel(
-        copyStart: getBool(json['copy_start']),
+        succeeded: getBool(json['copy_start']),
       );
 
   /// Copy start confirmation. Returns `true` is success.
-  final bool copyStart;
+  final bool succeeded;
 
   /// Generate a copy of instance with given parameters
   CopyTradingStartModel copyWith({
-    bool copyStart,
+    bool succeeded,
   }) =>
       CopyTradingStartModel(
-        copyStart: copyStart ?? this.copyStart,
+        succeeded: succeeded ?? this.succeeded,
       );
 }
--- lib/api/models/copy_trading_stop_model.dart
+++ lib/api/models/copy_trading_stop_model.dart
@@ -5,23 +5,23 @@ import 'package:flutter_deriv_api/utils/helpers.dart';
 class CopyTradingStopModel extends BaseModel {
   /// Initializes
   CopyTradingStopModel({
-    this.copyStart,
+    this.succeeded,
   });
 
   /// Generate an instance from json
   factory CopyTradingStopModel.fromJson(Map<String, dynamic> json) =>
       CopyTradingStopModel(
-        copyStart: getBool(json['copy_stop']),
+        succeeded: getBool(json['copy_stop']),
       );
 
   /// Copy start confirmation. Returns `true` is success.
-  final bool copyStart;
+  final bool succeeded;
 
   /// Generate a copy of instance with given parameters
   CopyTradingStopModel copyWith({
-    bool copyStart,
+    bool succeeded,
   }) =>
       CopyTradingStopModel(
-        copyStart: copyStart ?? this.copyStart,
+        succeeded: succeeded ?? this.succeeded,
       );
 }
