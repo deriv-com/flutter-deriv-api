bb15296e679cbd81fcb709222c04d704b34b2274
raminvakili-fs <55975218+raminvakili-fs@users.noreply.github.com>
2020-04-30 12:15:37 +0430

    ramin/add_copytrading_list_n_statistics (#76)
    * added copy trading classes
    
    * adding copy trading classes
    
    * completed copytrading list and statistics classes and tests
    
    * fix exception class
    
    * Some refactoring

---
+++ lib/api/models/market_trades_breakdown_model.dart
@@ -0,0 +1,32 @@
+import 'base_model.dart';
+
+/// Represents the portfolio distribution by [market].
+class MarketTradesBreakdownModel extends BaseModel {
+  /// Initializes
+  MarketTradesBreakdownModel({this.market, this.breakdown});
+
+  /// From JSON
+  factory MarketTradesBreakdownModel.fromJson(
+    MapEntry<String, dynamic> entry,
+  ) =>
+      MarketTradesBreakdownModel(
+        market: entry.key,
+        breakdown: entry.value,
+      );
+
+  /// Breakdown
+  final double breakdown;
+
+  /// Market name
+  final String market;
+
+  /// Clones a new instance
+  MarketTradesBreakdownModel copyWith({
+    double breakdown,
+    String market,
+  }) =>
+      MarketTradesBreakdownModel(
+        breakdown: breakdown ?? this.breakdown,
+        market: market ?? this.market,
+      );
+}
