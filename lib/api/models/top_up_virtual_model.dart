f679b83cd4e006f292d5d0b7f323968e182802f8
hamed-fs <57184669+hamed-fs@users.noreply.github.com>
2020-04-30 12:56:36 +0430

    hamed/add_top_up_virtual_class (#60)
    * hamed/add_top_up_virtual_class
    
    - add top up virtual class
    - add top up virtual unit test
    
    * hamed/minor_refactor
    
    - minor refactor

---
+++ lib/api/models/top_up_virtual_model.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_deriv_api/api/models/base_model.dart';
+
+/// Top-Up virtual model class
+abstract class TopUpVirtualModel extends BaseModel {
+  /// Class constructor
+  TopUpVirtualModel({
+    this.amount,
+    this.currency,
+  });
+
+  /// Top up amount
+  final double amount;
+
+  /// Top up currency string
+  final String currency;
+}
