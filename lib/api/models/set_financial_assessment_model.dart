8ecb8e41cf78f1df202be0dcc3a679d0aa1ddd5d
raminvakili-fs <55975218+raminvakili-fs@users.noreply.github.com>
2020-04-30 12:19:43 +0430

    ramin/add_set_financial_assessment (#75)
    * addign portfolio classes
    
    * took out portfolio (will be added in seperate branch)
    
    * Added set financial assessment

---
+++ lib/api/models/set_financial_assessment_model.dart
@@ -0,0 +1,48 @@
+import 'base_model.dart';
+
+///The financial assessment score assigned to the submitted financial assessment
+class SetFinancialAssessmentModel extends BaseModel {
+  /// Initializes
+  SetFinancialAssessmentModel({
+    this.cfdScore,
+    this.financialInformationScore,
+    this.totalScore,
+    this.tradingScore,
+  });
+
+  /// Generates a new instance from JSON
+  factory SetFinancialAssessmentModel.fromJson(Map<String, dynamic> json) =>
+      SetFinancialAssessmentModel(
+        cfdScore: json['cfd_score'],
+        financialInformationScore: json['financial_information_score'],
+        totalScore: json['total_score'],
+        tradingScore: json['trading_score'],
+      );
+
+  /// CFD score based on answers
+  final int cfdScore;
+
+  /// Financial information score based on answers
+  final int financialInformationScore;
+
+  /// Financial Assessment score based on answers
+  final int totalScore;
+
+  /// Trading experience score based on answers
+  final int tradingScore;
+
+  /// Creates a copy of instance with given parameters
+  SetFinancialAssessmentModel copyWith({
+    int cfdScore,
+    int financialInformationScore,
+    int totalScore,
+    int tradingScore,
+  }) =>
+      SetFinancialAssessmentModel(
+        cfdScore: cfdScore ?? this.cfdScore,
+        financialInformationScore:
+            financialInformationScore ?? this.financialInformationScore,
+        totalScore: totalScore ?? this.totalScore,
+        tradingScore: tradingScore ?? this.tradingScore,
+      );
+}
