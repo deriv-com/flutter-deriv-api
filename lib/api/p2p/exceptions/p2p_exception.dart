16d2b19a3e80e4663a32e5f37d82d6cb93158f16
hamed-fs <hamed@firstsource.io>
2020-04-30 11:17:46 +0430

    hamed/remove_p2p_exception_class
    - remove p2p exception class

--- lib/api/p2p/exceptions/p2p_exception.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_deriv_api/api/exceptions/api_base_exception.dart';
-
-/// P2P exception class
-class P2PException extends APIBaseException {
-  /// Class constructor
-  P2PException({String message}) : super(message: message);
-}
