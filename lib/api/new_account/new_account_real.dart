778558e5b0eb93031e698f674acb7be4eef4d4a0
hamed-fs <57184669+hamed-fs@users.noreply.github.com>
2020-04-30 12:54:27 +0430

    hamed/add_new_account_classes (#62)
    - add new account virtual class
    - add new account virtual unit test
    - add new account real class
    - add new account real unit test

---
+++ lib/api/new_account/new_account_real.dart
@@ -0,0 +1,39 @@
+import 'package:flutter_deriv_api/api/models/new_account_real_model.dart';
+
+/// New account real class
+class NewAccountReal extends NewAccountRealModel {
+  /// Initializes
+  NewAccountReal({
+    String clientId,
+    String landingCompany,
+    String landingCompanyShort,
+    String oauthToken,
+  }) : super(
+          clientId: clientId,
+          landingCompany: landingCompany,
+          landingCompanyShort: landingCompanyShort,
+          oauthToken: oauthToken,
+        );
+
+  /// Generate an instance from json
+  factory NewAccountReal.fromJson(Map<String, dynamic> json) => NewAccountReal(
+        clientId: json['client_id'],
+        landingCompany: json['landing_company'],
+        landingCompanyShort: json['landing_company_short'],
+        oauthToken: json['oauth_token'],
+      );
+
+  /// Generate a copy of instance with given parameters
+  NewAccountReal copyWith({
+    String clientId,
+    String landingCompany,
+    String landingCompanyShort,
+    String oauthToken,
+  }) =>
+      NewAccountReal(
+        clientId: clientId ?? this.clientId,
+        landingCompany: landingCompany ?? this.landingCompany,
+        landingCompanyShort: landingCompanyShort ?? this.landingCompanyShort,
+        oauthToken: oauthToken ?? this.oauthToken,
+      );
+}
