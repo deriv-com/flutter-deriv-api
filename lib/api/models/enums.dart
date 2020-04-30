4e33399da58172d897e8d9197ec20bde6a42d03d
raminvakili-fs <55975218+raminvakili-fs@users.noreply.github.com>
2020-04-30 14:52:38 +0430

    ramin/add_account_status (#50)
    * addign portfolio classes
    
    * Added account status classes
    
    * Some minor fixes
    
    * removed portfolio to add it on its own PR
    
    * minor fixes
    
    * added account status enums
    
    * getting json list from helper function
    
    * apply getListFromMap func
    
    * added TODO for potentional enum fields
    
    * added getFromMap func as well
    
    * turen to todos to uppercase
    
    * Applying EnumHelper for enum fields
    
    * fix copyWith doc

--- lib/api/models/enums.dart
+++ lib/api/models/enums.dart
@@ -1,3 +1,82 @@
+/// This represent the current status for proof of identity document
+/// submitted for authentication.
+enum AccountIdentityStatus {
+  /// None
+  none,
+
+  /// Document proof is pending
+  pending,
+
+  /// Document rejected
+  rejected,
+
+  /// Document verified
+  verified,
+
+  /// Document expired
+  expired,
+
+  /// Suspected document
+  suspected,
+}
+
+/// This represent the current status for proof of identity document
+/// submitted for authentication.
+enum AccountRiskClassification {
+  /// Low
+  low,
+
+  /// Standard
+  standard,
+
+  /// High
+  high,
+}
+
+/// Account status
+enum AccountStatusType {
+  /// client is age-verified.
+  ageVerification,
+
+  /// client is fully authenticated.
+  authenticated,
+
+  /// cashier is locked.
+  cashierLocked,
+
+  /// client has updated tax related information.
+  crsTinInformation,
+
+  /// account is disabled.
+  disabled,
+
+  /// client's submitted proof-of-identity documents have expired.
+  documentExpired,
+
+  /// client's submitted proof-of-identity documents are expiring within a month.
+  documentExpiringSoon,
+
+  /// client has not completed financial assessment.
+  financialAssessmentNotComplete,
+
+  /// client has not completed financial information.
+  financialInformationNotComplete,
+
+  /// this client has opted for a professional account.
+  professional,
+
+  /// Trading experience not completed
+  tradingExperienceNotComplete,
+
+  /// client has acknowledged UKGC funds protection notice.
+  ukgcFundsProtection,
+
+  /// client cannot deposit or buy contracts, but can withdraw or sell contracts.
+  unwelcome,
+
+  /// deposits allowed but withdrawals locked.
+  withdrawalLocked,
+}
 // Payment agent
 /// Payment result
 enum PaymentResult {
