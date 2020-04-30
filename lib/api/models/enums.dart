5927860569e204b2c98ee71b492031f35681fbc7
hamed-fs <57184669+hamed-fs@users.noreply.github.com>
2020-04-30 13:58:14 +0430

    hamed/add_payment_agent_classes (#52)
    * hamed/add_payment_agent_classes
    
    - add payment agent model
    - add payment agent unit test
    - add country model
    
    * hamed/add_payment_agent_transfer
    
    - add payment agent transfer class
    - add payment agent transfer unit test
    
    * hamed/add_payment_agent_withdraw_class
    
    - add payment agent withdraw class
    - add payment agent withdraw unit test

--- lib/api/models/enums.dart
+++ lib/api/models/enums.dart
@@ -1,3 +1,16 @@
+// Payment agent
+/// Payment result
+enum PaymentResult {
+  /// Noun, this value is not a valid value
+  noun,
+
+  /// Transfer success
+  success,
+
+  /// dry-run success
+  dryrunSuccess,
+}
+
 // MT5 enums
 /// Account type
 enum AccountType {
