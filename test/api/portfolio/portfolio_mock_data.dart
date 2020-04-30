56d4b6c3903a5c05f9dc8d0c6b2179b9511b4187
raminvakili-fs <55975218+raminvakili-fs@users.noreply.github.com>
2020-04-30 13:59:26 +0430

    ramin/add_portfolio_classes (#51)
    * addign portfolio classes
    
    * Added portfolio classes
    
    * Minor fix in field DateTime

---
+++ test/api/portfolio/portfolio_mock_data.dart
@@ -0,0 +1,25 @@
+const String portfolioJSON = '''{
+  "echo_req": {
+    "portfolio": 1
+  },
+  "msg_type": "portfolio",
+  "portfolio": {
+    "contracts": [
+      {
+        "app_id": 1,
+        "buy_price": 10,
+        "contract_id": 80080459208,
+        "contract_type": "PUT",
+        "currency": "USD",
+        "date_start": 1587634074,
+        "expiry_time": 1587761700,
+        "longcode": "Win payout if EUR/GBP is strictly lower than 0.87821 at close on 2020-04-24.",
+        "payout": 10.88,
+        "purchase_time": 1587634074,
+        "shortcode": "PUT_FRXEURGBP_10.88_1587634074_1587761700_878210_0",
+        "symbol": "frxEURGBP",
+        "transaction_id": 160058875308
+      }
+    ]
+  }
+}''';
