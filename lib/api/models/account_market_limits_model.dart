54890d8c729ccd3a7a77b359ef770dda0290bd9c
Ramin Vakili <ramin.vakili@firstsource.tech>
2020-05-03 10:16:35 +0430

    fix AccountMarketLimitsModel comment

--- lib/api/models/account_market_limits_model.dart
+++ lib/api/models/account_market_limits_model.dart
@@ -2,7 +2,7 @@ import 'package:flutter_deriv_api/api/models/market_limit_model.dart';
 
 import 'base_model.dart';
 
-/// Contains limitation information for each market.
+/// Contains limitation information for a market
 class AccountMarketLimitsModel extends BaseModel {
   /// Initializes
   AccountMarketLimitsModel({this.marketName, this.marketLimits});
