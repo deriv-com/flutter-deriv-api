1decccf8b8f07a8b563bb89a1d2dd1d0ad7f1a32
hamed-fs <57184669+hamed-fs@users.noreply.github.com>
2020-05-04 09:00:18 +0430

    hamed/add_p2p_advert_classes (#74)
    * hamed/add_p2p_advert_create_classes
    
    - add p2p advert create classes
    - add p2p advert unit test
    
    * hamed/add_p2p_advert_info_class
    
    - add p2p advert info class
    
    * hamed/minor_refactor
    
    - minor refactor
    
    * hamed/add_advertiser_class
    
    - add advertiser class
    - add advertiser unit test
    
    * hamed/remove_p2p_exception_class
    
    - remove p2p exception class

---
+++ lib/api/models/subscription_model.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_deriv_api/api/models/base_model.dart';
+
+/// Subscription model class
+class SubscriptionModel extends BaseModel {
+  /// Initializes
+  SubscriptionModel({
+    this.id,
+  });
+
+  /// Generate an instance from json
+  factory SubscriptionModel.fromJson(Map<String, dynamic> json) =>
+      SubscriptionModel(
+        id: json == null ? null : json['id'],
+      );
+
+  /// A per-connection unique identifier. Can be passed to the `forget` API call to unsubscribe.
+  final String id;
+
+  /// Generate a copy of instance with given parameters
+  SubscriptionModel copyWith({
+    String id,
+  }) =>
+      SubscriptionModel(
+        id: id ?? this.id,
+      );
+}
---
+++ test/api/p2p/p2p_advertiser/p2p_advertiser_model_mock_data.dart
@@ -0,0 +1,19 @@
+const String p2pAdvertiserModelMockData = '''
+{
+  "advertiser":{
+    "chat_token": "chat_token_652346562349223",
+    "chat_user_id": "p2p_user_CR_3_1587613550",
+    "contact_info": "contact details template",
+    "created_time": 1587613551,
+    "default_advert_description": "instructions template",
+    "id": "3",
+    "is_approved": 1,
+    "is_listed": 0,
+    "name": "advertiser CR90000019",
+    "payment_info": "payment instructions template"
+  },
+  "subscription": {
+    "id": "cd28a29d-6100-d530-9c5b-4e39bc2aa0a7"
+  }
+}
+''';
