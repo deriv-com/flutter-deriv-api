e2ae4318d393665c6cdfa52e7346e7fbbcf20e7e
Ramin Vakili <ramin.vakili@firstsource.tech>
2020-05-04 10:18:52 +0430

    add contract update history classes and unit test

---
+++ test/api/models/contract_update_history_mock_data.dart
@@ -0,0 +1,16 @@
+const String contractUpdateHistoryJSON = '''{
+  "contract_update_history": [
+    {
+      "display_name": "localized display name",
+      "order_amount": 430.0,
+      "order_date": 1587744006,
+      "order_type": "stop loss",
+      "value": "100.0"
+    }
+  ],
+  "echo_req": {
+    "contract_id": 60345301948,
+    "contract_update_history": 1
+  },
+  "msg_type": "contract_update_history"
+}''';
--- test/api/models/update_contract_model_test.dart
+++ test/api/models/update_contract_model_test.dart
@@ -2,8 +2,10 @@ import 'dart:convert';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_deriv_api/api/contracts/update_contract.dart';
+import 'package:flutter_deriv_api/api/models/history_spot_price_model.dart';
 import 'package:flutter_deriv_api/utils/helpers.dart';
 
+import 'contract_update_history_mock_data.dart';
 import 'update_contract_model_mock_data.dart';
 
 void main() {
@@ -22,5 +24,23 @@ void main() {
     expect(updateContract.takeProfit.orderAmount, 430.0);
     expect(updateContract.takeProfit.orderDate, getDateTime(1587744006));
     expect(updateContract.takeProfit.value, '100.0');
+  });
+
+  test('Contract update history JSON parsing', () {
+    final Map<String, dynamic> contractUpdateHistoryMap =
+        jsonDecode(contractUpdateHistoryJSON);
+
+    final List<HistorySpotPriceModel> updateHistory = getListFromMap(
+      contractUpdateHistoryMap['contract_update_history'],
+      (dynamic item) => HistorySpotPriceModel.fromJson(item),
+    );
+
+    final HistorySpotPriceModel firstUpdate = updateHistory.first;
+
+    expect(firstUpdate.displayName, 'localized display name');
+    expect(firstUpdate.orderAmount, 430.0);
+    expect(firstUpdate.orderDate, getDateTime(1587744006));
+    expect(firstUpdate.orderType, 'stop loss');
+    expect(firstUpdate.value, '100.0');
   });
 }
