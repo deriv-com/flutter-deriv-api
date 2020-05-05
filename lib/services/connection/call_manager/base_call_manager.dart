aa2f0967416f18f1edac0912d15ddf152cc6f9ee
hamed-fs <hamed@firstsource.io>
2020-05-05 14:17:50 +0430

    hamed/fix_subscription_manager_issue
    - fix subscription manager issue

--- lib/services/connection/call_manager/base_call_manager.dart
+++ lib/services/connection/call_manager/base_call_manager.dart
@@ -2,11 +2,12 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:meta/meta.dart';
 
+import 'package:flutter_deriv_api/basic_api/generated/api.helper.dart';
 import 'package:flutter_deriv_api/basic_api/request.dart';
 import 'package:flutter_deriv_api/basic_api/response.dart';
-import 'package:flutter_deriv_api/basic_api/generated/api.helper.dart';
-import 'package:flutter_deriv_api/services/connection/call_manager/pending_request.dart';
 import 'package:flutter_deriv_api/services/connection/basic_binary_api.dart';
+import 'package:flutter_deriv_api/services/connection/call_manager/pending_request.dart';
+import 'package:flutter_deriv_api/services/connection/call_manager/subscription_stream.dart';
 
 /// A predicate function to compare [request] and [pendingRequest]s
 /// [equatableResult] indicates request and pending request are equal or not (by equatable package result)
@@ -50,11 +51,9 @@ abstract class BaseCallManager<T> {
     @required int requestId,
     @required Map<String, dynamic> response,
   }) {
-    print('completing request for $requestId.');
-
     final Completer<Response> requestCompleter = _getResponse(requestId);
 
-    if (!requestCompleter.isCompleted) {
+    if (response['subscription'] == null && !requestCompleter.isCompleted) {
       requestCompleter.complete(getResponseByMsgType(response));
     }
   }
@@ -62,28 +61,25 @@ abstract class BaseCallManager<T> {
   /// Add [request] to pending requests queue, api history and web socket channel
   Future<Response> addToChannel({
     @required Request request,
-    Map<String, dynamic Function()> absentFields,
+    SubscriptionStream<Response> subscriptionStream,
   }) {
     final Completer<Response> response = Completer<Response>();
     final Request requestWithId = request.copyWith(reqId: _getRequestId());
-    final Map<String, dynamic> prepareRequest = _prepareRequest(requestWithId);
-
-    if (absentFields != null && absentFields.isNotEmpty) {
-      absentFields.forEach(
-        (String key, dynamic Function() function) =>
-            prepareRequest.putIfAbsent(key, function),
-      );
-    }
+    final Map<String, dynamic> prepareRequest = _prepareRequest(
+      request: requestWithId,
+      isSubscription: subscriptionStream != null,
+    );
 
     _addPendingRequest(
       request: requestWithId,
       response: response,
+      subscriptionStream: subscriptionStream,
     );
 
     api.apiHistory.pushOutgoing(
       timestamp: DateTime.now().millisecondsSinceEpoch,
+      method: requestWithId.msgType,
       message: prepareRequest,
-      method: request.msgType,
     );
 
     api.webSocketChannel.sink.add(
@@ -101,14 +97,27 @@ abstract class BaseCallManager<T> {
   void _addPendingRequest({
     @required Request request,
     @required Completer<Response> response,
+    SubscriptionStream<Response> subscriptionStream,
   }) =>
       _pendingRequests[request.reqId] = PendingRequest<Response>(
         request: request,
         response: response,
+        subscriptionStream: subscriptionStream,
       );
 
   int _getRequestId() => _requestId++;
 
-  Map<String, dynamic> _prepareRequest(Request request) => request.toJson()
-    ..removeWhere((String key, dynamic value) => value == null);
+  Map<String, dynamic> _prepareRequest({
+    Request request,
+    bool isSubscription,
+  }) {
+    final Map<String, dynamic> result = request.toJson()
+      ..removeWhere((String key, dynamic value) => value == null);
+
+    if (isSubscription) {
+      result.putIfAbsent('subscribe', () => 1);
+    }
+
+    return result;
+  }
 }
--- lib/services/connection/call_manager/pending_request.dart
+++ lib/services/connection/call_manager/pending_request.dart
@@ -1,22 +1,44 @@
 import 'dart:async';
 
 import 'package:flutter_deriv_api/basic_api/request.dart';
+import 'package:flutter_deriv_api/services/connection/call_manager/subscription_stream.dart';
 
 /// Represent a pending request.
 class PendingRequest<T> {
-  /// Class constructor
+  /// Initializes
   PendingRequest({
-    this.method,
     this.request,
     this.response,
+    this.subscriptionId,
+    this.subscriptionStream,
   });
-
-  /// Api call method (e.g. 'active_symbols')
-  final String method;
 
   /// Map of the sent request
   final Request request;
 
   /// This completer will complete whenever the response of the request arrives
   final Completer<T> response;
+
+  /// Subscription id
+  final String subscriptionId;
+
+  /// Subscription stream
+  final SubscriptionStream<T> subscriptionStream;
+
+  /// Get subscription state
+  bool get isSubscribed => subscriptionStream != null;
+
+  /// Creates a copy of instance with given parameters
+  PendingRequest<T> copyWith({
+    Request request,
+    Completer<T> response,
+    String subscriptionId,
+    SubscriptionStream<T> subscriptionStream,
+  }) =>
+      PendingRequest<T>(
+        request: request ?? this.request,
+        response: response ?? this.response,
+        subscriptionId: subscriptionId ?? this.subscriptionId,
+        subscriptionStream: subscriptionStream ?? this.subscriptionStream,
+      );
 }
