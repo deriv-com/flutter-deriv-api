36c11dd220540cf9b6e656d964eda6d4eef2a7be
hamed-fs <hamed@firstsource.io>
2020-05-11 15:09:04 +0430

    hamed/add_exchange_rates_call
    - add exchange rates call

--- test/api/common/exchange/exchange_rates_model_test.dart
+++ test/api/common/exchange/exchange_rates_model_test.dart
@@ -1,17 +1,19 @@
-import 'dart:convert';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_deriv_api/api/common/exchange/exchange_rates.dart';
 import 'package:flutter_deriv_api/api/common/models/rate_model.dart';
+import 'package:flutter_deriv_api/basic_api/generated/api.dart';
+import 'package:flutter_deriv_api/services/dependency_injector/injector.dart';
+import 'package:flutter_deriv_api/services/dependency_injector/module_container.dart';
 import 'package:flutter_deriv_api/utils/helpers.dart';
 
-import 'exchange_rates_model_mock_data.dart';
-
 void main() {
-  test('exchange rates JSON parsing', () {
-    final Map<String, dynamic> mapData = jsonDecode(exchangeRatesModelMockData);
-    final ExchangeRates exchangeRates =
-        ExchangeRates.fromJson(mapData['exchange_rates']);
+  test('fetch exchange rates', () async {
+    ModuleContainer().initialize(Injector.getInjector(), isMock: true);
+
+    final ExchangeRates exchangeRates = await ExchangeRates.fetchExchangeRates(
+      const ExchangeRatesRequest(baseCurrency: 'USD'),
+    );
 
     expect(exchangeRates.baseCurrency, 'USD');
     expect(exchangeRates.date, getDateTime(1588064134));
