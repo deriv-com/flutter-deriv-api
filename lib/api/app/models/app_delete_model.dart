a2503137e7297fa8cf036c0164226b45fe779ba1
hamed-fs <hamed@firstsource.io>
2020-05-13 09:29:24 +0430

    hamed/resolve_build_issue
    - resolve build issue

--- lib/api/app/models/app_delete_model.dart
+++ lib/api/app/models/app_delete_model.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_deriv_api/api/models/api_base_model.dart';
-import 'package:flutter_deriv_api/utils/helpers.dart';
 
 /// App delete model class
 class AppDeleteModel extends APIBaseModel {
