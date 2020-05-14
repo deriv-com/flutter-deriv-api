ee25fb7dfc9e18d523a27fd7f7e24f1eb71d03fe
Ramin Vakili <ramin.vakili@firstsource.tech>
2020-05-14 13:51:41 +0430

    added balance subscribe test

--- test/api/account/balance/balance_test.dart
+++ test/api/account/balance/balance_test.dart
@@ -6,15 +6,37 @@ import 'package:flutter_deriv_api/services/dependency_injector/injector.dart';
 import 'package:flutter_deriv_api/services/dependency_injector/module_container.dart';
 
 void main() {
-  test('Balance test', () async {
+  group('Balance group', () {
     ModuleContainer().initialize(Injector.getInjector(), isMock: true);
 
-    final Balance balance =
-        await Balance.fetchBalance(const BalanceRequest(account: 'current'));
+    test('Fetch balance', () async {
+      final Balance balance = await Balance.fetchBalance(
+        const BalanceRequest(
+          account: 'current',
+        ),
+      );
 
-    expect(balance.balance, 9650.74);
-    expect(balance.currency, 'USD');
-    expect(balance.id, '811e7ac4-1454-b909-6adb-904a2e9cb374');
-    expect(balance.loginId, 'VRTC2351953');
+      expect(balance.balance, 9650.74);
+      expect(balance.currency, 'USD');
+      expect(balance.id, '811e7ac4-1454-b909-6adb-904a2e9cb374');
+      expect(balance.loginId, 'VRTC2351953');
+    });
+
+    test('Subscribe balance', () {
+      Balance.subscribeBalance(const BalanceRequest(account: 'current')).listen(
+        expectAsync1(
+          (Balance balance) {
+            expect(balance.balance, 9650.74);
+            expect(balance.currency, 'USD');
+            expect(balance.id, '811e7ac4-1454-b909-6adb-904a2e9cb374');
+            expect(balance.loginId, 'VRTC2351953');
+            expect(
+              balance.subscriptionInformation.id,
+              '811e7ac4-1454-b909-6adb-904a2e9cb374',
+            );
+          },
+        ),
+      );
+    });
   });
 }
