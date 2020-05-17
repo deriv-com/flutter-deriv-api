3a449389ad7b98112554a48061d60c26d853de50
hamed-fs <hamed@firstsource.io>
2020-05-17 16:33:06 +0430

    hamed/add_copy_trading_api_calls
    - add copy trading calls
    - add copy trading unit tests

---
+++ lib/api/common/copy_trading/copy_trading_start.dart
@@ -0,0 +1,44 @@
+import 'package:flutter_deriv_api/api/common/copy_trading/exceptions/copy_trading_exception.dart';
+import 'package:flutter_deriv_api/api/common/models/copy_trading_start_model.dart';
+import 'package:flutter_deriv_api/basic_api/generated/api.dart';
+import 'package:flutter_deriv_api/services/connection/api_manager/base_api.dart';
+import 'package:flutter_deriv_api/services/dependency_injector/injector.dart';
+import 'package:flutter_deriv_api/utils/helpers.dart';
+
+/// Copy trading start class
+class CopyTradingStart extends CopyTradingStartModel {
+  /// Initializes
+  CopyTradingStart({
+    bool succeeded,
+  }) : super(succeeded: succeeded);
+
+  /// Generate an instance from response
+  factory CopyTradingStart.fromResponse(int result) => CopyTradingStart(
+        succeeded: getBool(result),
+      );
+
+  static final BaseAPI _api = Injector.getInjector().get<BaseAPI>();
+
+  /// Generate a copy of instance with given parameters
+  CopyTradingStart copyWith({
+    bool succeeded,
+  }) =>
+      CopyTradingStart(
+        succeeded: succeeded ?? this.succeeded,
+      );
+
+  /// Starts copy trader bets.
+  /// For parameters information refer to [CopyStartRequest].
+  static Future<CopyTradingStart> start(CopyStartRequest request) async {
+    final CopyStartResponse response = await _api.call(request: request);
+
+    checkException(
+      response: response,
+      exceptionCreator: (String message) => CopyTradingException(
+        message: message,
+      ),
+    );
+
+    return CopyTradingStart.fromResponse(response.copyStart);
+  }
+}
---
+++ lib/services/connection/api_manager/mock_data/common/copy_stop_response.dart
@@ -0,0 +1,10 @@
+/// Copy stop response
+const String copyStopResponse = '''
+{
+  "echo_req": {
+    "copy_stop": "<not shown>"
+  },
+  "copy_stop": 1,
+  "msg_type": "copy_stop"
+}
+''';
