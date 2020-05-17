b4195db1ed47b211bd377099a15c4be8cafd9f48
hamed-fs <hamed@firstsource.io>
2020-05-17 14:18:12 +0430

    hamed/add_authorize_api_call
    - add authorize api call
    - add authorize unit test

--- test/api/account/authorize/authorize_test.dart
+++ test/api/account/authorize/authorize_test.dart
@@ -1,32 +1,43 @@
-import 'dart:convert';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_deriv_api/api/account/authorize/account.dart';
+import 'package:flutter_deriv_api/api/account/account.dart';
 import 'package:flutter_deriv_api/api/account/authorize/authorize.dart';
 import 'package:flutter_deriv_api/api/account/models/local_currency_model.dart';
-
-import 'authorize_model_mock_data.dart';
+import 'package:flutter_deriv_api/basic_api/generated/api.dart';
+import 'package:flutter_deriv_api/services/dependency_injector/injector.dart';
+import 'package:flutter_deriv_api/services/dependency_injector/module_container.dart';
 
 void main() {
-  test('authorize JSON parsing', () {
-    final Map<String, dynamic> mapData = jsonDecode(authorizeModelMockData);
-    final Authorize authorize = Authorize.fromJson(mapData['authorize']);
+  test('authorize', () async {
+    ModuleContainer().initialize(Injector.getInjector(), isMock: true);
+
+    final Authorize authorize = await Authorize.authorize(
+      const AuthorizeRequest(
+        authorize: 'sample_token_334da73d',
+        addToLoginHistory: 1,
+      ),
+    );
+
     final List<Account> accounts = authorize.accountList;
     final List<LocalCurrencyModel> localCurrencies = authorize.localCurrencies;
 
     expect(accounts.length, 2);
 
-    expect(accounts[0].currency, 'USD');
-    expect(accounts[1].excludedUntil,
-        DateTime.fromMillisecondsSinceEpoch(1587486726000));
-    expect(accounts[0].isDisabled, false);
-    expect(accounts[0].isVirtual, false);
-    expect(accounts[0].landingCompanyName, 'svg');
-    expect(accounts[0].loginId, 'CR90000028');
+    expect(accounts.first.currency, 'USD');
+    expect(
+      accounts[1].excludedUntil,
+      DateTime.fromMillisecondsSinceEpoch(1587486726000),
+    );
+    expect(accounts.first.isDisabled, false);
+    expect(accounts.first.isVirtual, false);
+    expect(accounts.first.landingCompanyName, 'svg');
+    expect(accounts.first.loginId, 'CR90000028');
 
     expect(accounts[1].currency, 'USD');
-    expect(accounts[1].excludedUntil,
-        DateTime.fromMillisecondsSinceEpoch(1587486726000));
+    expect(
+      accounts[1].excludedUntil,
+      DateTime.fromMillisecondsSinceEpoch(1587486726000),
+    );
     expect(accounts[1].isDisabled, false);
     expect(accounts[1].isVirtual, true);
     expect(accounts[1].landingCompanyName, 'virtual');
@@ -34,8 +45,8 @@ void main() {
 
     expect(localCurrencies.length, 2);
 
-    expect(localCurrencies[0].currencyCode, 'ZAR');
-    expect(localCurrencies[0].fractionalDigits, 2);
+    expect(localCurrencies.first.currencyCode, 'ZAR');
+    expect(localCurrencies.first.fractionalDigits, 2);
 
     expect(localCurrencies[1].currencyCode, 'USD');
     expect(localCurrencies[1].fractionalDigits, 3);
