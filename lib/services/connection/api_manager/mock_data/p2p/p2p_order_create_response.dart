f0ac9d7739e73a7a235a18e5f821488ed839ee1b
Ramin Vakili <ramin.vakili@firstsource.tech>
2020-05-17 10:08:49 +0430

    Added order API calls and Mock test

---
+++ lib/services/connection/api_manager/mock_data/p2p/p2p_order_create_response.dart
@@ -0,0 +1,41 @@
+/// Order create response
+const String p2pOrderCreateResponse = '''{
+  "echo_req": {
+    "advert_id": "8",
+    "amount": 50,
+    "p2p_order_create": 1
+  },
+  "msg_type": "p2p_order_create",
+  "p2p_order_create": {
+    "account_currency": "USD",
+    "advert_details": {
+      "description": "Please contact via whatsapp 1234",
+      "id": "8",
+      "payment_method": "bank_transfer",
+      "type": "sell"
+    },
+    "advertiser_details": {
+      "id": "6",
+      "name": "advertiser CR90000018"
+    },
+    "amount": 50,
+    "amount_display": "50.00",
+    "chat_channel_url": "",
+    "contact_info": "Please contact via whatsapp 1234",
+    "created_time": 1589688096,
+    "expiry_time": 1589695296,
+    "id": "107",
+    "is_incoming": 0,
+    "local_currency": "ZAR",
+    "payment_info": "Transfer to account 000-1111",
+    "price": 675000,
+    "price_display": "675000.00",
+    "rate": 13500,
+    "rate_display": "13500.00",
+    "status": "pending",
+    "type": "buy"
+  },
+  "subscription": {
+    "id": "857cfc96-1014-66ce-9c49-0a4dbd22857a"
+  }
+}''';
