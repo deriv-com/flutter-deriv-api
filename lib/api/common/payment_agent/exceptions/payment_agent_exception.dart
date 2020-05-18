074193d41e042e29d1af50c0c7b9a906b5637f59
hamed-fs <hamed@firstsource.io>
2020-05-18 14:43:27 +0430

    hamed/add_payment_agent_api_calls
    - add payment agent api calls
    - add payment agent unit tests

---
+++ lib/api/common/payment_agent/exceptions/payment_agent_exception.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_deriv_api/api/common/exceptions/common_exception.dart';
+
+/// Payment agent exception
+class PaymentAgentException extends CommonException {
+  /// Initializes
+  PaymentAgentException({String message}) : super(message: message);
+}
