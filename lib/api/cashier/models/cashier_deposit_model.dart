9452dd4d34c9f6f60a581324e35ab7727f430fa6
Ramin Vakili <ramin.vakili@firstsource.tech>
2020-06-01 17:58:11 +0430

    Minor refactor

--- lib/api/cashier/models/cashier_deposit_model.dart
+++ lib/api/cashier/models/cashier_deposit_model.dart
@@ -3,13 +3,13 @@ import 'package:flutter_deriv_api/api/models/api_base_model.dart';
 /// Cashier Deposit Information model class
 class CashierDepositModel extends APIBaseModel {
   /// Initializes
-  CashierDepositModel(this.address);
+  CashierDepositModel({this.address});
 
   /// Creates an instance from JSON
   factory CashierDepositModel.fromJson(
     Map<String, dynamic> json,
   ) =>
-      CashierDepositModel(json['address']);
+      CashierDepositModel(address: json['address']);
 
   /// Address for crypto deposit.
   final String address;
@@ -18,5 +18,5 @@ class CashierDepositModel extends APIBaseModel {
   CashierDepositModel copyWith(
     String address,
   ) =>
-      CashierDepositModel(address ?? this.address);
+      CashierDepositModel(address: address ?? this.address);
 }
