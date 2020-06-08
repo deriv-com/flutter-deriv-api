b9ca4ae7f822e063294226fb794d7397190fd380
Ramin Vakili <ramin.vakili@firstsource.tech>
2020-06-08 13:19:57 +0430

    apply review comment

--- lib/api/common/residence/residence.dart
+++ lib/api/common/residence/residence.dart
@@ -33,10 +33,8 @@ class Residence extends ResidenceModel {
         countryName: json['text'],
         countryCode: json['value'],
         tinFormat: getListFromMap(json['tin_format']),
-        disabled: json['disabled'] != null &&
-            (json['disabled'] == 'disabled' || json['disabled'] == 'DISABLED'),
-        selected: json['selected'] != null &&
-            (json['selected'] == 'selected' || json['selected'] == 'SELECTED'),
+        disabled: json['disabled']?.toString()?.toLowerCase() == 'disabled',
+        selected: json['selected']?.toString()?.toLowerCase() == 'selected',
       );
 
   static final BaseAPI _api = Injector.getInjector().get<BaseAPI>();
