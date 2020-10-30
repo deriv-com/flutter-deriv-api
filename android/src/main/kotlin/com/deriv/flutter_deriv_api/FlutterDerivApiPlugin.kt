6eb13192273cf1af2b2f102d8608decc6694de7b
Suyash Mohan <suyash@binary.com>
2020-10-30 17:44:59 +0800

    upgrade to embedding v2

--- android/src/main/kotlin/com/deriv/flutter_deriv_api/FlutterDerivApiPlugin.kt
+++ android/src/main/kotlin/com/deriv/flutter_deriv_api/FlutterDerivApiPlugin.kt
@@ -1,25 +1,50 @@
 package com.deriv.flutter_deriv_api
 
-import io.flutter.plugin.common.MethodCall
+import android.content.Context
+import androidx.annotation.NonNull
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
+import io.flutter.plugin.common.PluginRegistry
 
-class FlutterDerivApiPlugin: MethodCallHandler {
+class FlutterDerivApiPlugin: FlutterPlugin, MethodCallHandler {
   companion object {
+    const val CHANNEL_NAME = "flutter_deriv_api"
+
     @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "flutter_deriv_api")
-      channel.setMethodCallHandler(FlutterDerivApiPlugin())
+    fun registerWith(registrar: PluginRegistry.Registrar) {
+      val instance = FlutterDerivApiPlugin()
+      instance.setupChannel(registrar.messenger())
     }
   }
 
-  override fun onMethodCall(call: MethodCall, result: Result) {
+  private lateinit var channel: MethodChannel
+
+  override fun onAttachedToEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    setupChannel(binding.getBinaryMessenger())
+  }
+
+  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    teardownChannel();
+  }
+
+  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
     if (call.method == "getPlatformVersion") {
       result.success("Android ${android.os.Build.VERSION.RELEASE}")
     } else {
       result.notImplemented()
     }
+  }
+
+  private fun setupChannel(messenger: BinaryMessenger) {
+    channel = MethodChannel(messenger, CHANNEL_NAME)
+    channel.setMethodCallHandler(this)
+  }
+
+  private fun teardownChannel() {
+    channel.setMethodCallHandler(null)
   }
 }
