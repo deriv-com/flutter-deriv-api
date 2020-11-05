07116447343071f277d90535658a4a150ddf5941
hamed-fs <57184669+hamed-fs@users.noreply.github.com>
2020-11-05 12:02:37 +0330

    hamed/update_profit_table_response (#147)
    * hamed/update_websocket_submodule
    
    - update websocket submodule
    
    * hamed/update_profit_table_response
    
    - update profit table response

--- lib/services/connection/api_manager/mock_data/account/profit_table_response.dart
+++ lib/services/connection/api_manager/mock_data/account/profit_table_response.dart
@@ -9,8 +9,20 @@ const String profitTableResponse = '''
     "sort": "ASC"
   },
   "profit_table": {
-    "count": 1,
+    "count": 2,
     "transactions": [
+      {
+        "app_id": 1234,
+        "buy_price": 250.0,
+        "contract_id": 4867502908,
+        "longcode": "Win 50% of your stake for every 1% fall in USD/JPY.",
+        "payout": 10.0,
+        "purchase_time": 1587544006,
+        "sell_price": 5.25,
+        "sell_time": 1587554006,
+        "shortcode": "MULTDOWN_FRXUSDJPY_7_50_1603975474_4757575474_60m_0.00",
+        "transaction_id": 10867502908
+      },
       {
         "app_id": 1234,
         "buy_price": 250.0,
