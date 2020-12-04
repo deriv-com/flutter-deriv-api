5fccd1e6cd6c283887cad517ff9ed97ce728a857
abedelaziz <abed@deriv.com>
2020-12-04 15:09:02 +0800

    Attempt to fix connectivity issue.

--- lib/state/connection/connection_bloc.dart
+++ lib/state/connection/connection_bloc.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:flutter_deriv_api/api/api_initializer.dart';
-import 'package:flutter_deriv_api/api/common/ping/ping.dart';
 import 'package:flutter_deriv_api/services/connection/api_manager/base_api.dart';
 import 'package:flutter_deriv_api/services/connection/api_manager/connection_information.dart';
 import 'package:flutter_deriv_api/services/connection/connection_service.dart';
@@ -36,10 +35,8 @@ class ConnectionBloc extends Bloc<ConnectionEvent, ConnectionState> {
       (internet_bloc.InternetState state) {
         if (state is internet_bloc.Disconnected) {
           add(Disconnect());
-          _stopPingTimer();
         } else if (state is internet_bloc.Connected) {
           _connectWebSocket();
-          _startPingTimer();
         }
       },
     );
@@ -48,7 +45,6 @@ class ConnectionBloc extends Bloc<ConnectionEvent, ConnectionState> {
   /// Creates mock connection, sets this to [true] for testing purposes
   final bool isMock;
 
-  static const Duration _pingInterval = Duration(seconds: 12);
   static const Duration _callTimeOut = Duration(seconds: 10);
 
   BaseAPI _api;
@@ -56,8 +52,6 @@ class ConnectionBloc extends Bloc<ConnectionEvent, ConnectionState> {
   StreamSubscription<internet_bloc.InternetState> _internetListener;
 
   internet_bloc.InternetBloc _internetBloc;
-
-  Timer _pingTimer;
 
   final UniqueKey _uniqueKey = UniqueKey();
 
@@ -69,8 +63,6 @@ class ConnectionBloc extends Bloc<ConnectionEvent, ConnectionState> {
   Stream<ConnectionState> mapEventToState(ConnectionEvent event) async* {
     if (event is Connect && state is! Connected) {
       yield Connected();
-
-      _startPingTimer();
     } else if (event is Reconnect || event is Reconfigure) {
       if (event is Reconfigure) {
         _connectionInformation = event.connectionInformation;
@@ -102,7 +94,6 @@ class ConnectionBloc extends Bloc<ConnectionEvent, ConnectionState> {
         // Needed to reset state after changing the endpoint from settings page
         yield InitialConnectionState();
       }
-
     } else if (event is Disconnect) {
       if (state is Connected) {
         await _api.disconnect();
@@ -141,22 +132,4 @@ class ConnectionBloc extends Bloc<ConnectionEvent, ConnectionState> {
 
     return super.close();
   }
-
-  void _startPingTimer() {
-    if (_pingTimer == null || !_pingTimer.isActive) {
-      _pingTimer = Timer.periodic(_pingInterval, (Timer timer) async {
-        try {
-          await Ping.ping().timeout(_callTimeOut);
-        } on Exception catch (e) {
-          if (state is! Reconnecting) {
-            timer.cancel();
-            add(Reconnect());
-          }
-          dev.log(e.toString(), error: e);
-        }
-      });
-    }
-  }
-
-  void _stopPingTimer() => _pingTimer?.cancel();
 }
