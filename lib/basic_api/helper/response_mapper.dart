c4f1a14f80b9b93b26418bef270836cd1b6a02db
Mohammad Teimouri <teimouri.mohammad@firstsource.tech>
2021-02-21 16:58:07 +0330

    update analysis

--- lib/basic_api/helper/response_mapper.dart
+++ lib/basic_api/helper/response_mapper.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_deriv_api/api/manually/ohlc_receive.dart';
-import 'package:flutter_deriv_api/api/response/ticks_history_receive_result.dart';
 import 'package:flutter_deriv_api/basic_api/generated/ticks_history_receive.dart';
 
 import '../response.dart';
