0de3628ccd684e32211b6f1c994ab7ece56bc927
bibinmpanicker <bibin@deriv.com>
2021-09-02 18:04:58 +0530

    refactored

--- lib/services/connection/api_manager/binary_api.dart
+++ lib/services/connection/api_manager/binary_api.dart
@@ -9,7 +9,7 @@ import 'package:flutter_deriv_api/api/models/enums.dart';
 import 'package:flutter_deriv_api/basic_api/generated/api.dart';
 import 'package:flutter_deriv_api/basic_api/request.dart';
 import 'package:flutter_deriv_api/basic_api/response.dart';
-import 'package:flutter_deriv_api/helpers/miscellaneous_helper.dart';
+import 'package:flutter_deriv_api/helpers/helpers.dart';
 import 'package:flutter_deriv_api/services/connection/api_manager/base_api.dart';
 import 'package:flutter_deriv_api/services/connection/api_manager/connection_information.dart';
 import 'package:flutter_deriv_api/services/connection/call_manager/base_call_manager.dart';
