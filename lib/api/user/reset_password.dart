2198fa0c91c53de4fd4d47f020f39205b4ec486d
Farzin Mirzaie <farzin@firstsource.tech>
2021-10-11 15:18:15 +0330

    Fix build issue

--- lib/api/user/reset_password.dart
+++ lib/api/user/reset_password.dart
@@ -1,8 +1,7 @@
 import 'package:flutter_deriv_api/api/models/base_exception_model.dart';
 import 'package:flutter_deriv_api/api/user/exceptions/user_exception.dart';
 import 'package:flutter_deriv_api/api/user/models/reset_pass_model.dart';
-import 'package:flutter_deriv_api/basic_api/manually/reset_password_send.dart';
-import 'package:flutter_deriv_api/basic_api/manually/reset_password_receive.dart';
+import 'package:flutter_deriv_api/basic_api/generated/api.dart';
 import 'package:flutter_deriv_api/helpers/helpers.dart';
 import 'package:flutter_deriv_api/services/connection/api_manager/base_api.dart';
 import 'package:flutter_deriv_api/services/dependency_injector/injector.dart';
