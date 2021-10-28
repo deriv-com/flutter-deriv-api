0893f7e67c8fbebdb665cbfb72b970bde8d45a4d
Farzin Mirzaie <72082844+farzin-fs@users.noreply.github.com>
2021-10-28 11:47:46 +0330

    Farzin/add missing models (#185)
    * Run build_runner
    
    * Add service_token
    
    * Add p2p_order_dispute
    
    * Export new models
    
    * Fix P2pAdvertiserInfoRequest
    
    * Fix P2pAdvertiserInfoRequest
    
    * Add manual json files
    
    * Update setup.sh script
    
    * Run build_runner
    
    * Add missing endline
    
    * Fix build issue

--- lib/basic_api/generated/p2p_order_dispute_receive.dart
+++ lib/basic_api/generated/p2p_order_dispute_receive.dart
@@ -1,15 +1,14 @@
-/// Generated automatically from flutter_deriv_api|lib/basic_api/generated/p2p_advert_info_receive.json.
+/// Generated automatically from flutter_deriv_api|lib/basic_api/generated/p2p_order_dispute_receive.json.
 
 // ignore_for_file: always_put_required_named_parameters_first
 
 import '../response.dart';
 
-/// P2p advert info response class.
-class P2pAdvertInfoResponse extends Response {
-  /// Initialize P2pAdvertInfoResponse.
-  const P2pAdvertInfoResponse({
-    this.p2pAdvertInfo,
-    this.subscription,
+/// P2p order dispute response class.
+class P2pOrderDisputeResponse extends Response {
+  /// Initialize P2pOrderDisputeResponse.
+  const P2pOrderDisputeResponse({
+    this.p2pOrderDispute,
     Map<String, dynamic>? echoReq,
     Map<String, dynamic>? error,
     String? msgType,
@@ -22,27 +21,22 @@ class P2pAdvertInfoResponse extends Response {
         );
 
   /// Creates an instance from JSON.
-  factory P2pAdvertInfoResponse.fromJson(Map<String, dynamic> json) =>
-      P2pAdvertInfoResponse(
-        p2pAdvertInfo: json['p2p_advert_info'] as Map<String, dynamic>?,
-        subscription: json['subscription'] as Map<String, dynamic>?,
+  factory P2pOrderDisputeResponse.fromJson(Map<String, dynamic> json) =>
+      P2pOrderDisputeResponse(
+        p2pOrderDispute: json['p2p_order_dispute'] as Map<String, dynamic>?,
         echoReq: json['echo_req'] as Map<String, dynamic>?,
         error: json['error'] as Map<String, dynamic>?,
         msgType: json['msg_type'] as String?,
         reqId: json['req_id'] as int?,
       );
 
-  /// P2P advert information.
-  final Map<String, dynamic>? p2pAdvertInfo;
-
-  /// For subscription requests only.
-  final Map<String, dynamic>? subscription;
+  /// Details of the disputed order.
+  final Map<String, dynamic>? p2pOrderDispute;
 
   /// Converts this instance to JSON
   @override
   Map<String, dynamic> toJson() => <String, dynamic>{
-        'p2p_advert_info': p2pAdvertInfo,
-        'subscription': subscription,
+        'p2p_order_dispute': p2pOrderDispute,
         'echo_req': echoReq,
         'error': error,
         'msg_type': msgType,
@@ -51,17 +45,15 @@ class P2pAdvertInfoResponse extends Response {
 
   /// Creates a copy of instance with given parameters
   @override
-  P2pAdvertInfoResponse copyWith({
-    Map<String, dynamic>? p2pAdvertInfo,
-    Map<String, dynamic>? subscription,
+  P2pOrderDisputeResponse copyWith({
+    Map<String, dynamic>? p2pOrderDispute,
     Map<String, dynamic>? echoReq,
     Map<String, dynamic>? error,
     String? msgType,
     int? reqId,
   }) =>
-      P2pAdvertInfoResponse(
-        p2pAdvertInfo: p2pAdvertInfo ?? this.p2pAdvertInfo,
-        subscription: subscription ?? this.subscription,
+      P2pOrderDisputeResponse(
+        p2pOrderDispute: p2pOrderDispute ?? this.p2pOrderDispute,
         echoReq: echoReq ?? this.echoReq,
         error: error ?? this.error,
         msgType: msgType ?? this.msgType,
--- lib/basic_api/generated/reset_password_receive.dart
+++ lib/basic_api/generated/reset_password_receive.dart
@@ -1,15 +1,14 @@
-/// Generated automatically from flutter_deriv_api|lib/basic_api/generated/p2p_advert_info_receive.json.
+/// Generated automatically from flutter_deriv_api|lib/basic_api/generated/reset_password_receive.json.
 
 // ignore_for_file: always_put_required_named_parameters_first
 
 import '../response.dart';
 
-/// P2p advert info response class.
-class P2pAdvertInfoResponse extends Response {
-  /// Initialize P2pAdvertInfoResponse.
-  const P2pAdvertInfoResponse({
-    this.p2pAdvertInfo,
-    this.subscription,
+/// Reset password response class.
+class ResetPasswordResponse extends Response {
+  /// Initialize ResetPasswordResponse.
+  const ResetPasswordResponse({
+    this.resetPassword,
     Map<String, dynamic>? echoReq,
     Map<String, dynamic>? error,
     String? msgType,
@@ -22,27 +21,27 @@ class P2pAdvertInfoResponse extends Response {
         );
 
   /// Creates an instance from JSON.
-  factory P2pAdvertInfoResponse.fromJson(Map<String, dynamic> json) =>
-      P2pAdvertInfoResponse(
-        p2pAdvertInfo: json['p2p_advert_info'] as Map<String, dynamic>?,
-        subscription: json['subscription'] as Map<String, dynamic>?,
+  factory ResetPasswordResponse.fromJson(Map<String, dynamic> json) =>
+      ResetPasswordResponse(
+        resetPassword:
+            json['reset_password'] == null ? null : json['reset_password'] == 1,
         echoReq: json['echo_req'] as Map<String, dynamic>?,
         error: json['error'] as Map<String, dynamic>?,
         msgType: json['msg_type'] as String?,
         reqId: json['req_id'] as int?,
       );
 
-  /// P2P advert information.
-  final Map<String, dynamic>? p2pAdvertInfo;
-
-  /// For subscription requests only.
-  final Map<String, dynamic>? subscription;
+  /// `true`: password reset success, `false`: password reset failure
+  final bool? resetPassword;
 
   /// Converts this instance to JSON
   @override
   Map<String, dynamic> toJson() => <String, dynamic>{
-        'p2p_advert_info': p2pAdvertInfo,
-        'subscription': subscription,
+        'reset_password': resetPassword == null
+            ? null
+            : resetPassword!
+                ? 1
+                : 0,
         'echo_req': echoReq,
         'error': error,
         'msg_type': msgType,
@@ -51,17 +50,15 @@ class P2pAdvertInfoResponse extends Response {
 
   /// Creates a copy of instance with given parameters
   @override
-  P2pAdvertInfoResponse copyWith({
-    Map<String, dynamic>? p2pAdvertInfo,
-    Map<String, dynamic>? subscription,
+  ResetPasswordResponse copyWith({
+    bool? resetPassword,
     Map<String, dynamic>? echoReq,
     Map<String, dynamic>? error,
     String? msgType,
     int? reqId,
   }) =>
-      P2pAdvertInfoResponse(
-        p2pAdvertInfo: p2pAdvertInfo ?? this.p2pAdvertInfo,
-        subscription: subscription ?? this.subscription,
+      ResetPasswordResponse(
+        resetPassword: resetPassword ?? this.resetPassword,
         echoReq: echoReq ?? this.echoReq,
         error: error ?? this.error,
         msgType: msgType ?? this.msgType,
