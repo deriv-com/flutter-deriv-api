abe566e9a9510c3b9cf064789f648a76341ebcd3
Farzin Mirzaie <72082844+farzin-fs@users.noreply.github.com>
2022-03-30 08:06:33 +0430

    Farzin/Exchange rates (#201)
    * Update models
    
    * Update generated models

--- lib/basic_api/generated/exchange_rates_receive.dart
+++ lib/basic_api/generated/exchange_rates_receive.dart
@@ -9,6 +9,7 @@ class ExchangeRatesResponse extends Response {
   /// Initialize ExchangeRatesResponse.
   const ExchangeRatesResponse({
     this.exchangeRates,
+    this.subscription,
     Map<String, dynamic>? echoReq,
     Map<String, dynamic>? error,
     String? msgType,
@@ -24,6 +25,7 @@ class ExchangeRatesResponse extends Response {
   factory ExchangeRatesResponse.fromJson(Map<String, dynamic> json) =>
       ExchangeRatesResponse(
         exchangeRates: json['exchange_rates'] as Map<String, dynamic>?,
+        subscription: json['subscription'] as Map<String, dynamic>?,
         echoReq: json['echo_req'] as Map<String, dynamic>?,
         error: json['error'] as Map<String, dynamic>?,
         msgType: json['msg_type'] as String?,
@@ -33,10 +35,14 @@ class ExchangeRatesResponse extends Response {
   /// Exchange rate values from base to all other currencies
   final Map<String, dynamic>? exchangeRates;
 
+  /// For subscription requests only.
+  final Map<String, dynamic>? subscription;
+
   /// Converts this instance to JSON
   @override
   Map<String, dynamic> toJson() => <String, dynamic>{
         'exchange_rates': exchangeRates,
+        'subscription': subscription,
         'echo_req': echoReq,
         'error': error,
         'msg_type': msgType,
@@ -47,6 +53,7 @@ class ExchangeRatesResponse extends Response {
   @override
   ExchangeRatesResponse copyWith({
     Map<String, dynamic>? exchangeRates,
+    Map<String, dynamic>? subscription,
     Map<String, dynamic>? echoReq,
     Map<String, dynamic>? error,
     String? msgType,
@@ -54,6 +61,7 @@ class ExchangeRatesResponse extends Response {
   }) =>
       ExchangeRatesResponse(
         exchangeRates: exchangeRates ?? this.exchangeRates,
+        subscription: subscription ?? this.subscription,
         echoReq: echoReq ?? this.echoReq,
         error: error ?? this.error,
         msgType: msgType ?? this.msgType,
