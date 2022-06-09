5978a70abbf50501323112caf207ae00a2660cf8
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2022-06-09 10:23:12 +0800

    hamed/refactor_binary_api

--- lib/services/connection/api_manager/base_api.dart
+++ lib/services/connection/api_manager/base_api.dart
@@ -7,44 +7,45 @@ import 'package:flutter_deriv_api/basic_api/response.dart';
 import 'package:flutter_deriv_api/services/connection/api_manager/connection_information.dart';
 import 'package:flutter_deriv_api/services/connection/call_manager/base_call_manager.dart';
 
-/// Callbacks for websocket connection
+/// Callbacks for websocket connection.
 typedef ConnectionCallback = void Function(UniqueKey uniqueKey);
 
-/// Base class for handling API connection and calling APIs
+/// Base class for handling API connection and calling APIs.
 abstract class BaseAPI {
-  /// Initializes
+  /// Initializes base api.
   BaseAPI(this.uniqueKey);
 
   /// A key to check the `onDone` function is called from the same instance.
   final UniqueKey uniqueKey;
 
-  /// Connects to API
+  /// Connects to API.
   Future<void> connect(
     ConnectionInformation? connectionInformation, {
     ConnectionCallback? onDone,
     ConnectionCallback? onOpen,
     ConnectionCallback? onError,
+    bool printResponse,
   });
 
-  /// Adds request to stream channel
+  /// Adds request to stream channel.
   void addToChannel(Map<String, dynamic> request);
 
-  /// Calls a API method by [request]
+  /// Calls a API method by [request].
   Future<T> call<T>({required Request request});
 
-  /// Subscribe to a [request]
+  /// Subscribe to a [request].
   /// [comparePredicate] indicates compare condition for current [request] and [pendingRequest]s
   Stream<Response>? subscribe({
     required Request request,
     RequestCompareFunction? comparePredicate,
   });
 
-  /// Unsubscribe with a specific [subscriptionId]
+  /// Unsubscribe with a specific [subscriptionId].
   Future<ForgetResponse> unsubscribe({required String? subscriptionId});
 
-  /// Unsubscribe to multiple [method]s all at once
+  /// Unsubscribe to multiple [method]s all at once.
   Future<ForgetAllResponse?> unsubscribeAll({required ForgetStreamType method});
 
-  /// Disconnects from API
+  /// Disconnects from API.
   Future<void> disconnect();
 }
