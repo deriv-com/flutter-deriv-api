017846d463d7d13667b3e8b3fbe44def1beb00b4
Hamed Rezaee <57184669+hamed-deriv@users.noreply.github.com>
2022-11-02 12:49:56 +0800

    hamed/fix_api_disconnect_issue (#228)
    - fix api disconnect issue

--- test/services/dependency_injector/injector_test.dart
+++ test/services/dependency_injector/injector_test.dart
@@ -10,8 +10,8 @@ void main() {
   tearDown(() => Injector.getInjector().dispose());
 
   test('Injector Test', () {
-    final BaseAPI? _api = Injector.getInjector().get<BaseAPI>();
+    final BaseAPI? api = Injector.getInjector().get<BaseAPI>();
 
-    expect(_api is MockAPI, true);
+    expect(api is MockAPI, true);
   });
 }
