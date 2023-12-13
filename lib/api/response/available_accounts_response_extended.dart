686a96bb0706fb120bd09da0816b92a348356e95
akhil-deriv <akhil@regentmarkets.com>
2023-12-13 09:19:06 +0400

    added available accounts response extended

---
+++ lib/api/response/available_accounts_response_extended.dart
@@ -0,0 +1,28 @@
+import 'package:deriv_dependency_injector/dependency_injector.dart';
+import 'package:flutter_deriv_api/api/exceptions/exceptions.dart';
+import 'package:flutter_deriv_api/api/response/available_accounts_response_result.dart';
+import 'package:flutter_deriv_api/basic_api/generated/available_accounts_receive.dart';
+import 'package:flutter_deriv_api/basic_api/generated/available_accounts_send.dart';
+import 'package:flutter_deriv_api/helpers/helpers.dart';
+import 'package:flutter_deriv_api/services/connection/api_manager/base_api.dart';
+
+/// The extended version of the [AvailableAccountsResponse] class to implement
+/// the API call methods.
+class AvailableAccountsResponseExtended extends AvailableAccountsResponse {
+  static final BaseAPI _api = Injector()<BaseAPI>();
+
+  /// Fetches the cashier payments.
+  static Future<AvailableAccountsResponse> fetchCashierPayments({
+    required AvailableAccountsRequest request,
+  }) async {
+    final AvailableAccountsReceive response = await _api.call(request: request);
+
+    checkException(
+      response: response,
+      exceptionCreator: ({BaseExceptionModel? baseExceptionModel}) =>
+          BaseAPIException(baseExceptionModel: baseExceptionModel),
+    );
+
+    return AvailableAccountsResponse.fromJson(response.availableAccounts);
+  }
+}
