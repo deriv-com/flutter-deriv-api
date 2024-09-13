964c8c6dc140af4143a14d200dc732c62209602b
WAQAS YOUNAS <waqas.younas@regentmarkets.com>
2024-09-13 17:17:38 +0400

    Update trading_platform_status_response_result.dart

--- lib/api/response/trading_platform_status_response_result.dart
+++ lib/api/response/trading_platform_status_response_result.dart
@@ -1,14 +1,8 @@
 // ignore_for_file: prefer_single_quotes, unnecessary_import, unused_import
 
-import 'package:deriv_dependency_injector/dependency_injector.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter_deriv_api/basic_api/generated/trading_platform_status_receive.dart';
-import 'package:flutter_deriv_api/basic_api/generated/trading_platform_status_send.dart';
 
 import 'package:flutter_deriv_api/helpers/helpers.dart';
-
-import '../../services/connection/api_manager/base_api.dart';
-import '../exceptions/exceptions.dart';
 
 /// Trading platform status response model class.
 abstract class TradingPlatformStatusResponseModel {
@@ -27,30 +21,6 @@ class TradingPlatformStatusResponse extends TradingPlatformStatusResponseModel {
   const TradingPlatformStatusResponse({
     required super.tradingPlatformStatus,
   });
-
-  static final BaseAPI _api = Injector()<BaseAPI>();
-
-  /// Trading Platform: New Account (request).
-  ///
-  /// This call gets the status of external trading accounts, either demo or real money.
-  /// For parameters information refer to [TradingPlatformStatusRequest].
-  /// Throws a [BaseAPIException] if API response contains an error.
-  static Future<TradingPlatformStatusResponse> getStatus(
-    TradingPlatformStatusRequest request,
-  ) async {
-    final TradingPlatformStatusReceive response =
-        await _api.call(request: request);
-
-    checkException(
-      response: response,
-      exceptionCreator: ({BaseExceptionModel? baseExceptionModel}) =>
-          BaseAPIException(baseExceptionModel: baseExceptionModel),
-    );
-
-    return TradingPlatformStatusResponse.fromJson(
-      response.tradingPlatformStatus,
-    );
-  }
 
   /// Creates an instance from JSON.
   factory TradingPlatformStatusResponse.fromJson(
