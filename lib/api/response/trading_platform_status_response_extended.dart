67ad8bf11185d4ec52fc2c9e8663a3df1a48d7d8
WAQAS YOUNAS <waqas.younas@regentmarkets.com>
2024-09-16 10:31:51 +0400

    Update trading_platform_status_response_extended.dart

--- lib/api/response/trading_platform_status_response_extended.dart
+++ lib/api/response/trading_platform_status_response_extended.dart
@@ -1,9 +1,10 @@
 import 'package:deriv_dependency_injector/dependency_injector.dart';
 import 'package:flutter_deriv_api/api/exceptions/exceptions.dart';
 import 'package:flutter_deriv_api/api/response/trading_platform_status_response_result.dart';
-import 'package:flutter_deriv_api/basic_api/generated/api.dart';
+import 'package:flutter_deriv_api/basic_api/generated/trading_platform_status_receive.dart';
 import 'package:flutter_deriv_api/helpers/helpers.dart';
 import 'package:flutter_deriv_api/services/connection/api_manager/base_api.dart';
+import 'package:flutter_deriv_api/basic_api/generated/trading_platform_status_send.dart';
 
 /// The extended version of the [TradingPlatformPasswordResetResponse] class to implement
 /// the API call methods.
