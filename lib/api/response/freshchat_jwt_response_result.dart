d4d7ba872a058677086c52500e6378bea4b1d611
sahani-deriv <sahani@deriv.com>
2024-10-02 16:38:55 +0800

    replace auth jwt with user jwt

--- lib/api/response/freshchat_jwt_response_result.dart
+++ lib/api/response/freshchat_jwt_response_result.dart
@@ -7,27 +7,27 @@ import 'package:flutter_deriv_api/helpers/helpers.dart';
 import 'package:flutter_deriv_api/services/connection/api_manager/base_api.dart';
 import 'package:deriv_dependency_injector/dependency_injector.dart';
 
-/// Freshworks Auth JWT response model class.
+/// Freshworks User JWT response model class.
 abstract class FreshChatJwtResponseModel {
   /// Initializes FreshChat JWT response model class.
   const FreshChatJwtResponseModel({
-    this.freshworksAuthJwt,
+    this.freshworksUserJwt,
   });
 
-  /// The Freshworks Auth JWT.
-  final FreshworksAuthJwt? freshworksAuthJwt;
+  /// The Freshworks User JWT.
+  final FreshworksUserJwt? freshworksUserJwt;
 }
 
-/// Freshworks Auth JWT.
-class FreshworksAuthJwt {
-  /// Initializes Freshworks Auth JWT.
-  const FreshworksAuthJwt({
+/// Freshworks User JWT.
+class FreshworksUserJwt {
+  /// Initializes Freshworks User JWT.
+  const FreshworksUserJwt({
     required this.token,
   });
 
   /// Creates an instance from JSON.
-  factory FreshworksAuthJwt.fromJson(Map<String, dynamic> json) =>
-      FreshworksAuthJwt(
+  factory FreshworksUserJwt.fromJson(Map<String, dynamic> json) =>
+      FreshworksUserJwt(
         token: json['token'],
       );
 
@@ -40,24 +40,24 @@ class FreshworksAuthJwt {
       };
 }
 
-/// Freshworks Auth JWT response class.
+/// Freshworks User JWT response class.
 class FreshChatJwtResponse extends FreshChatJwtResponseModel {
   /// Initializes FreshChat JWT response class.
   const FreshChatJwtResponse({
-    required FreshworksAuthJwt freshworksAuthJwt,
-  }) : super(freshworksAuthJwt: freshworksAuthJwt);
+    required FreshworksUserJwt freshworksUserJwt,
+  }) : super(freshworksUserJwt: freshworksUserJwt);
 
   /// Creates an instance from JSON.
   factory FreshChatJwtResponse.fromJson(Map<String, dynamic> json) =>
       FreshChatJwtResponse(
-        freshworksAuthJwt:
-            FreshworksAuthJwt.fromJson(json['freshworks_auth_jwt']),
+        freshworksUserJwt:
+            FreshworksUserJwt.fromJson(json['freshworks_user_jwt']),
       );
 
   /// Converts an instance to JSON.
   Map<String, dynamic> toJson() => <String, dynamic>{
         'service_token': <String, Map<String, dynamic>?>{
-          'freshworks_auth_jwt': freshworksAuthJwt?.toJson(),
+          'freshworks_user_jwt': freshworksUserJwt?.toJson(),
         },
       };
 
@@ -86,9 +86,9 @@ class FreshChatJwtResponse extends FreshChatJwtResponseModel {
 
   /// Creates a copy of instance with given parameters.
   FreshChatJwtResponse copyWith({
-    FreshworksAuthJwt? freshworksAuthJwt,
+    FreshworksUserJwt? freshworksUserJwt,
   }) =>
       FreshChatJwtResponse(
-        freshworksAuthJwt: freshworksAuthJwt ?? this.freshworksAuthJwt!,
+        freshworksUserJwt: freshworksUserJwt ?? this.freshworksUserJwt!,
       );
 }
