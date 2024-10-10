39e67f6ea3c123fd3c003e1ad44d90046730632d
Naif-Deriv <naif@regentmarkets.com>
2024-10-10 10:51:14 +0400

    replace single quotes with double quotes.

--- lib/services/connection/api_manager/mock_data/user/identity_verification_document_add_response.dart
+++ lib/services/connection/api_manager/mock_data/user/identity_verification_document_add_response.dart
@@ -2,9 +2,9 @@
 const String identityVerificationDocumentAddResponse = '''
 {
   "echo_req": {
-    "documentType": 'test',
-    "documentNumber": '123',
-    "issuingCountry": 'US'
+    "documentType": "test",
+    "documentNumber": "123",
+    "issuingCountry": "US"
   },
   "identity_verification_document_add": 1,
   "msg_type": "identity_verification_document_add"
