7177434bb4e82307365d22495c1b7cfe623071dd
Naif-Deriv <naif@regentmarkets.com>
2024-10-10 09:06:41 +0400

    add mock and test.

---
+++ lib/services/connection/api_manager/mock_data/user/identity_verification_document_add_response.dart
@@ -0,0 +1,12 @@
+/// Identity Verification Document Add Response
+const String identityVerificationDocumentAddResponse = '''
+{
+  "echo_req": {
+    "documentType": 'test',
+    "documentNumber": '123',
+    "issuingCountry": 'US',
+  },
+  "identity_verification_document_add": 1,
+  "msg_type": "identity_verification_document_add"
+}
+''';
