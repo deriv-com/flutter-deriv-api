f0fdb8f216ddfd5aaabfca62890fb66b2599c509
raslan-deriv <raslan@regentmarkets.com>
2024-12-27 11:06:12 +0400

    updated boolean comparision condition

--- lib/helpers/boolean_helper.dart
+++ lib/helpers/boolean_helper.dart
@@ -1,6 +1,6 @@
 /// Converts int to boolean
 bool? getBool(dynamic value) =>
-    value == null ? null : value == 1 || value == true;
+    value == null ? null : value == 1 || value == true || value == 'true';
 
 /// Converts boolean to int
 int? getInt({bool? value}) => value == null
